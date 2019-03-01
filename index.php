2906ac45a06150319ce33d40dc6bfe21933aea86
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-01 21:54:48 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
+
+
+
+
 echo "merhaba korkut";
+
+require_once('vendor/autoload.php');
+use \Brightzone\GremlinDriver\Connection;
+error_reporting(E_ALL ^ E_WARNING);
+// Write your own configuration values here
+$db = new Connection([
+    'host' => 'https://ratetaxicosmosdb.documents.azure.com.graphs.azure.com',
+    'username' => '/dbs/ratetaxicosmosdb/colls/ratetaxicosmosdb',
+    'password' => '6mpwHynjZvssEL8ejxI1CsA0eHn5uF2Z7YnZQdKRastGm5IvuNbLcLF4wwpZq2s8sDvMUg59RgR91RycsJjyHA=='
+    ,'port' => '443'
+    // Required parameter
+    ,'ssl' => TRUE
+]);
+
+
+echo "merhaba korkut2";
+
+
+
 
 ?> 
  
