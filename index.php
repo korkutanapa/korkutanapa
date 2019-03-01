19bf68c9ec98e81d453d8db9b3816d9a4518c1f6
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-01 21:41:14 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
 	$db = new Connection([
     'host' => 'https://ratetaxicosmosdb.documents.azure.com.graphs.azure.com',
     'username' => '/dbs/ratetaxicosmosdb/colls/ratetaxicosmosdb',
-    'password' => '6mpwHynjZvssEL8ejxI1CsA0eHn5uF2Z7YnZQdKRastGm5IvuNbLcLF4wwpZq2s8sDvMUg59RgR91RycsJjyHA=='
-    ,'port' => '443'
-    // Required parameter
-    ,'ssl' => TRUE
+    'password' => '6mpwHynjZvssEL8ejxI1CsA0eHn5uF2Z7YnZQdKRastGm5IvuNbLcLF4wwpZq2s8sDvMUg59RgR91RycsJjyHA==',
+    'port' => '443',
+    'ssl' => TRUE
 	]);
 
 
