bf9f02979659a80610f9d358889fd5419ecc63f7
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-01 21:44:24 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-	require_once('vendor/autoload.php');
-	use BrightzoneGremlinDriverConnection;
+require_once('vendor/autoload.php');
+use \Brightzone\GremlinDriver\Connection;
+error_reporting(E_ALL ^ E_WARNING);
 
-	$db = new Connection([
+$db = new Connection([
     'host' => 'https://ratetaxicosmosdb.documents.azure.com.graphs.azure.com',
     'username' => '/dbs/ratetaxicosmosdb/colls/ratetaxicosmosdb',
-    'password' => '6mpwHynjZvssEL8ejxI1CsA0eHn5uF2Z7YnZQdKRastGm5IvuNbLcLF4wwpZq2s8sDvMUg59RgR91RycsJjyHA==',
-    'port' => '443',
-    'ssl' => TRUE
-	]);
+    'password' => '6mpwHynjZvssEL8ejxI1CsA0eHn5uF2Z7YnZQdKRastGm5IvuNbLcLF4wwpZq2s8sDvMUg59RgR91RycsJjyHA=='
+    ,'port' => '443'
+    // Required parameter
+    ,'ssl' => TRUE
+]);
+$db->timeout = 0.5; 
 
 
 
