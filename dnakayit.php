5be74c1793745bc65a8fea5438129127066aeb89
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-07 18:48:56 +0300

    Add files via upload

--- dnakayit.php
+++ dnakayit.php
@@ -1,7 +1,21 @@
-<?php
-// YENİ ÜYE KAYIT DOSYASINDAN ALINAN VERİ DATABASE E YAZILIYOR
 
-include ("baglan.php");
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:reporttaxidbserver.database.windows.net,1433; Database = ReportTaxiDB", "becks", "774761Ka.");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "becks@reporttaxidbserver", "pwd" => "774761Ka.", "Database" => "ReportTaxiDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:reporttaxidbserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
 
 $cekoKullaniciAdi=$_POST["oKullaniciAdi"];
 $cekopassword=$_POST["opassword"];
