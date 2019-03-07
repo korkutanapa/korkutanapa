fe8338637a1bf098de1b17fe6b95f67e8bdd41c1
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-07 19:11:09 +0300

    Add files via upload

--- dnakayit.php
+++ dnakayit.php
@@ -1,14 +1,10 @@
 
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:reporttaxidbserver.database.windows.net,1433; Database = ReportTaxiDB", "becks", "774761Ka.");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+
+$connectionInfo = array("UID" => "becks@reporttaxidbserver", "pwd" => "774761Ka.", "Database" => "ReportTaxiDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:reporttaxidbserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
 $cekoKullaniciAdi=$_POST["oKullaniciAdi"];
 $cekopassword=$_POST["opassword"];
@@ -28,6 +24,7 @@ $sql = "INSERT INTO [dbo].[kisiler] (KullaniciAdi,sifre,AdSoyad,email,biografi,o
 if (mysqli_query($conn,$sql)) {
 	header('Location:index.html');
 	
+
  
 }else  {echo $sql . " uyumsuz kayıt lütfen istenen bilgileri giriniz";}
 }
