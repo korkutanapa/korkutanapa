cf302682c959171bc7c83c12ea885f3b13e266fe
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-07 22:50:39 +0300

    Add files via upload

---
+++ dnakayit.php
@@ -0,0 +1,32 @@
+<?php
+// YENİ ÜYE KAYIT DOSYASINDAN ALINAN VERİ DATABASE E YAZILIYOR
+
+include ("baglan.php");
+
+$cekoKullaniciAdi=$_POST["oKullaniciAdi"];
+$cekopassword=$_POST["opassword"];
+$cekoAdSoyad=$_POST["oAdSoyad"];
+$cekoEPosta=$_POST["oEPosta"];
+$cekoBiografi=$_POST["oBiografi"];
+
+if(empty($cekoKullaniciAdi) or empty($cekopassword) or empty($cekoAdSoyad)or empty($cekoEPosta) or empty($cekoBiografi)) {
+	echo "boş bırakmayınız" ;
+}
+else {
+
+$sql = "INSERT INTO kisiler (KullaniciAdi,sifre,AdSoyad,email,biografi,onay)
+ VALUES ('$cekoKullaniciAdi','$cekopassword','$cekoAdSoyad','$cekoEPosta','$cekoBiografi','0')";
+
+ 
+ 
+if (mysqli_query($conn, $sql)) {
+	header('Location:indexdna.html');
+	
+	
+ 
+}else  {echo " uyumsuz kayıt lütfen istenen bilgileri giriniz";}
+}
+
+mysqli_close($conn);
+
+?>
