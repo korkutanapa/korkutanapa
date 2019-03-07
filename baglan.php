572647f1ae5ceb6ee1d742ede1ea0489e9746844
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-07 23:57:08 +0300

    Add files via upload

--- baglan.php
+++ baglan.php
@@ -1,9 +1,17 @@
 <?php
-//Data Source=tcp:mssmssdb.database.windows.net,1433;Initial Catalog=mssdb;User Id=korkutanapa@mssmssdb.database.windows.net;Password=774761Ka.;
+
 
 $connectionInfo = array("UID" => "korkutanapa@mssmssdb", "pwd" => "774761Ka.", "Database" => "mssdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:mssmssdb.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+if (mysqli_query($conn)) 
+{
+echo " bağlan da oldu bu iş";
+}
+
+else  
+
+{echo "bağlan ad bağlanmadı";}
 
 ?>
 
