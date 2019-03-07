27d3f6911a4e0ae953a63fb926ca1df8d79e40cc
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-07 13:45:10 +0300

    Add files via upload

--- dnabaglan.php
+++ dnabaglan.php
@@ -1,17 +1,16 @@
+
 <?php
-$servername = "localhost";
-$username = "root";
- $password = "";
 
+try {
+    $conn = new PDO("sqlsrv:server = tcp:reporttaxidbserver.database.windows.net,1433; Database = ReportTaxiDB", "becks", "774761Ka.");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-// Create connection
- $conn = mysqli_connect($servername, $username, $password);
- $veri = mysqli_select_db ($conn,"dna");
-if (!$veri) {
-	echo "Baglantida problem var";
-	}
-
+$connectionInfo = array("UID" => "becks@reporttaxidbserver", "pwd" => "774761Ka.", "Database" => "ReportTaxiDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:reporttaxidbserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?> 
-
-
-
