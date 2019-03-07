83921bd7f835a1ee5067b28246a8d03dbb7ebd3b
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-08 00:02:48 +0300

    Add files via upload

--- baglan.php
+++ baglan.php
@@ -4,6 +4,28 @@
 $connectionInfo = array("UID" => "korkutanapa@mssmssdb", "pwd" => "774761Ka.", "Database" => "mssdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:mssmssdb.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+
+if ($conn->connect_errno) {
+    printf("Connect failed: %s\n", $conn->connect_error);
+    exit();
+}
+
+if (!$conn->query("SET a=1")) {
+    printf("Errormessage: %s\n", $mysqli->error);
+}
+
+/* close connection */
+$conn->close();
+
+
+
+
+
+
+
+
 if (mysqli_query($conn)) 
 {
 echo " bağlan da oldu bu iş";
