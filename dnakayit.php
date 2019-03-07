751978cdc1acc3295a1165fe283d7637e748cc06
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-07 19:07:13 +0300

    Add files via upload

--- dnakayit.php
+++ dnakayit.php
@@ -10,13 +10,6 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "becks@reporttaxidbserver", "pwd" => "774761Ka.", "Database" => "ReportTaxiDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:reporttaxidbserver.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
-
-
 $cekoKullaniciAdi=$_POST["oKullaniciAdi"];
 $cekopassword=$_POST["opassword"];
 $cekoAdSoyad=$_POST["oAdSoyad"];
@@ -31,13 +24,10 @@ else {
 $sql = "INSERT INTO [dbo].[kisiler] (KullaniciAdi,sifre,AdSoyad,email,biografi,onay)
  VALUES ('$cekoKullaniciAdi','$cekopassword','$cekoAdSoyad','$cekoEPosta','$cekoBiografi','0')";
 
- echo $conn." ".$sql;
- 
  
 if (mysqli_query($conn,$sql)) {
 	header('Location:index.html');
 	
-
  
 }else  {echo $sql . " uyumsuz kayıt lütfen istenen bilgileri giriniz";}
 }
