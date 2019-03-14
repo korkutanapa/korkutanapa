41bf302da116feaa6c33fce8a17272480e9ad335
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-14 21:59:01 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <Title>Report The Taxi Web Application</Title>
 </head>
 <body>
+
 <form method="post" action="?action=add" enctype="multipart/form-data" >
     Taxi Plate <input type="text" name="t_a" id="t_a"/></br>
     Point <input type="text" name="t_b" id="t_b"/></br>
@@ -13,6 +14,16 @@
 	Feedback_Compliant  <input type="text" name="t_f" id="t_f"/></br>
     <input type="submit" name="submit" value="Submit" />
 </form>
+
+
+<form method="post" action="?action=add1" enctype="multipart/form-data" >
+    id_trip <input type="text" name="t_g" id="t_g"/></br>
+    wheater <input type="text" name="t_h" id="t_h"/></br>
+
+    <input type="submit" name="submit" value="Submit" />
+</form>
+
+
 <?php
 /*Connect using SQL Server authentication.*/
 
@@ -25,6 +36,7 @@ if($conn === false)
 {
     die(print_r(sqlsrv_errors(), true));
 }
+
 if(isset($_GET['action']))
 {
     if($_GET['action'] == 'add')
@@ -60,6 +72,45 @@ if(isset($_GET['action']))
         }
     }
 }
+
+
+
+if(isset($_GET['action']))
+{
+    if($_GET['action'] == 'add1')
+    {
+        /*Insert data.*/
+     $insertSql = "INSERT INTO [dbo].[trip] ([id_trip],[weather]) VALUES (?,?)";
+     $params = array(	&$_POST['t_g'],
+                        &$_POST['t_h']
+					);
+					
+        $stmt = sqlsrv_query($conn, $insertSql, $params);
+        if($stmt === false)
+        {
+            /*Handle the case of a duplicte e-mail address.*/
+            $errors = sqlsrv_errors();
+            if($errors[0]['code'] == 2601)
+            {
+                echo "The e-mail address you entered has already been used.</br>";
+            }
+            /*Die if other errors occurred.*/
+            else
+            {
+                die(print_r($errors, true));
+            }
+        }
+        else
+        {
+            echo "Registration complete.</br>";
+        }
+    }
+}
+
+
+
+
+
 /*Display registered people.*/
 $sql = "SELECT * FROM kisiler ORDER BY KullaniciAdi";
 $stmt = sqlsrv_query($conn, $sql);
