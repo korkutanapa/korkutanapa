25fb451783632326ee6b5c5b5a739b78b7fa7a09
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-28 21:56:02 +0300

    Add files via upload

--- query.php
+++ query.php
@@ -83,15 +83,18 @@ if(isset($_GET['action']))
 
 // Make a MySQL Connection
 
-$query = "SELECT AVG(star) AS star_var FROM trip WHERE taxiplate='$a'"; 
+$query = "SELECT AVG(star) FROM trip WHERE taxiplate='$a'"; 
 	 
-$result = mysql_query($conn,$query) or die(mysql_error());
+$result = mysql_query($query) or die(mysql_error());
 
 // Print out result
-while($row = mysql_fetch_array($result)){
-	echo "The average value  of ". $row['star_var']. " is $".$row['AVG(star)'];
+while($row = mysql_fetch_array($result))
+
+{
+	echo "The average price of ". $a. " is $".$row['AVG(star)'];
 	echo "<br />";
 }
+
 ?>
 
 
