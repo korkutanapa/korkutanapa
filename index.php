0b6b7ac6ceb02299f99a59eca7656e90908c9616
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-28 20:31:18 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,28 +1,68 @@
 
+<!-- AÇILIŞ SAYFASI-->
 <html>
-<head>
-<Title>Report The Taxi Web Application</Title>
-</head>
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/></head>
+
+<!-- INTERNAL STYLE SHEET İLE SAYFA TASARIMI-->
+<style type="text/css">
+#flex-kutu{
+width:auto;
+height:auto;
+display:flex;
+border:2px solid silver;
+flex-direction:row;
+background-color:#FFA500;
+}
+
+
+h1 {
+font-size: 80px;
+font-family: Comic Sans MS;
+color: blue;
+text-align:center;
+}
+
+p{
+margin-left: 50px;
+font-size: 30px;
+font-family: comic sans ms;
+color: black;
+}
+.alt-kutular{
+width:1500;
+margin:30px;
+padding:5px;
+background-color:#D3D3D3;
+background-repeat:no-repeat;
+
+}
+
+.alt-kutularana{
+width:1500;
+margin:30px;
+padding:5px;
+background-color:#D3D3D3;
+background-image: url("ELLER11.jpg");
+background-position:center;
+background-repeat:no-repeat;
+}
+
+</style>
+
+<!-- SAYFA DA BÖLÜMLER FLEX KUTU İLE YAPILDI. ALT KUTULAR İLE SAYFALAR BÖLÜNDÜ-->
 <body>
+<div id ="flex-kutu">
+<div class="alt-kutularana">
+</div>
+
+
+<div class="alt-kutular">
 
 <form method="post" action="?action=add" enctype="multipart/form-data" >
     Taxi Plate <input type="text" name="t_a" id="t_a"/></br>
     Point <input type="text" name="t_b" id="t_b"/></br>
-    In Location <input type="text" name="t_c" id="t_c"/></br>
-    Out Location <input type="text" name="t_d" id="t_d"/></br>
-	Weather <input type="text" name="t_e" id="t_e"/></br>
-	Feedback_Compliant  <input type="text" name="t_f" id="t_f"/></br>
-    <input type="submit" name="submit" value="Submit" />
+        <input type="submit" name="submit" value="Submit" />
 </form>
-
-
-<form method="post" action="?action=add1" enctype="multipart/form-data" >
-    id_trip <input type="text" name="t_g" id="t_g"/></br>
-    wheater <input type="text" name="t_h" id="t_h"/></br>
-
-    <input type="submit" name="submit" value="Submit" />
-</form>
-
 
 <?php
 /*Connect using SQL Server authentication.*/
@@ -42,13 +82,10 @@ if(isset($_GET['action']))
     if($_GET['action'] == 'add')
     {
         /*Insert data.*/
-     $insertSql = "INSERT INTO [dbo].[kisiler] ([KullaniciAdi],[sifre],[AdSoyad],[email],[biografi],[onay]) VALUES (?,?,?,?,?,?)";
+     $insertSql = "INSERT INTO [dbo].[trip] ([taxiplate],[star]) VALUES (?,?)";
      $params = array(	&$_POST['t_a'],
-                        &$_POST['t_b'],
-                        &$_POST['t_c'],
-                        &$_POST['t_d'],
-						&$_POST['t_e'],
-                        &$_POST['t_f']
+             
+                        &$_POST['t_b']
 					);
 					
         $stmt = sqlsrv_query($conn, $insertSql, $params);
@@ -72,47 +109,10 @@ if(isset($_GET['action']))
         }
     }
 }
-
-
-
-if(isset($_GET['action']))
-{
-    if($_GET['action'] == 'add1')
-    {
-        /*Insert data.*/
-     $insertSql = "INSERT INTO [dbo].[weather] ([id_trip],[weather]) VALUES (?,?)";
-     $params = array(	&$_POST['t_g'],
-                        &$_POST['t_h']
-					);
-					
-        $stmt = sqlsrv_query($conn, $insertSql, $params);
-        if($stmt === false)
-        {
-            /*Handle the case of a duplicte e-mail address.*/
-            $errors = sqlsrv_errors();
-            if($errors[0]['code'] == 2601)
-            {
-                echo "The e-mail address you entered has already been used.</br>";
-            }
-            /*Die if other errors occurred.*/
-            else
-            {
-                die(print_r($errors, true));
-            }
-        }
-        else
-        {
-            echo "Registration complete.</br>";
-        }
-    }
-}
-
-
-
 
 
 /*Display registered people.*/
-$sql = "SELECT * FROM kisiler ORDER BY KullaniciAdi";
+$sql = "SELECT * FROM trip ORDER BY taxiplate";
 $stmt = sqlsrv_query($conn, $sql);
 if($stmt === false)
 {
@@ -123,25 +123,24 @@ if(sqlsrv_has_rows($stmt))
     print("<table border='1px'>");
     print("<tr><td>Taxi Plate</td>");
     print("<td>Point</td>");
-    print("<td>In Location</td>");
-	print("<td>Out Location</td>");
-	print("<td>Weather</td>");
-    print("<td>Feedback_Complaint</td></tr>");
-     
+       
     while($row = sqlsrv_fetch_array($stmt))
     {
          
-        print("<tr><td>".$row['KullaniciAdi']."</td>");
-        print("<td>".$row['sifre']."</td>");
-        print("<td>".$row['AdSoyad']."</td>");
-		print("<td>".$row['biografi']."</td>");
-		print("<td>".$row['onay']."</td>");
-        print("<td>".$row['email']."</td></tr>");
+        print("<tr><td>".$row['taxiplate']."</td>");
+     
+        print("<td>".$row['star']."</td></tr>");
     }
     print("</table>");
 }
 
 
 ?>
+
+
 </body>
+</div>
+</div>
 </html>
+
+
