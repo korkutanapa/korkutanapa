bb601c12417a29f3049b231b6ed28e3624fa2b0a
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-29 16:29:01 +0300

    Add files via upload

---
+++ registration_a.php
@@ -0,0 +1,32 @@
+<?php
+// YENİ ÜYE KAYIT DOSYASINDAN ALINAN VERİ DATABASE E YAZILIYOR
+
+include ("connection.php");
+
+$cekoKullaniciAdi=$_POST["oKullaniciAdi"];
+$cekopassword=$_POST["opassword"];
+$cekoAdSoyad=$_POST["oAdSoyad"];
+$cekoEPosta=$_POST["oEPosta"];
+$cekoBiografi=$_POST["oBiografi"];
+
+if(empty($cekoKullaniciAdi) or empty($cekopassword) or empty($cekoAdSoyad)or empty($cekoEPosta) or empty($cekoBiografi)) {
+	echo "boş bırakmayınız" ;
+}
+else {
+
+$sql = "INSERT INTO users (username,password,name_surname,phone_no,id_no,approve)
+ VALUES ('$cekoKullaniciAdi','$cekopassword','$cekoAdSoyad','$cekoEPosta','$cekoBiografi','0')";
+
+ 
+ 
+if (mysqli_query($conn, $sql)) {
+	header('Location:index.php');
+	
+	
+ 
+}else  {echo " please enter values correctly";}
+}
+
+mysqli_close($conn);
+
+?>
