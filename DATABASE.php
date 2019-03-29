e8ea074d0729f30a4602ce3bd8bdc581ade0abfa
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-29 09:30:11 +0300

    Add files via upload

---
+++ DATABASE.php
@@ -0,0 +1,113 @@
+
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/></head>
+
+<style type="text/css">
+#flex-kutu{
+width:auto;
+height:auto;
+display:flex;
+border:2px solid silver;
+flex-direction:row;
+background-color:#FFA500;
+}
+
+
+h1 {
+font-size: 80px;
+font-family: Comic Sans MS;
+color: blue;
+text-align:center;
+}
+
+p{
+margin-left: 50px;
+font-size: 30px;
+font-family: comic sans ms;
+color: black;
+}
+.alt-kutular{
+width:1500;
+margin:30px;
+padding:5px;
+background-color:#D3D3D3;
+background-repeat:no-repeat;
+
+}
+
+.alt-kutularana{
+width:1500;
+margin:30px;
+padding:5px;
+background-color:#D3D3D3;
+background-image: url("ELLER11.jpg");
+background-position:center;
+background-repeat:no-repeat;
+}
+
+</style>
+
+<body>
+<div id ="flex-kutu">
+
+<div class="alt-kutularana">
+
+<h3>TAXI DATABASE OF THE WORLD   </h3>
+
+</div>
+
+
+<div class="alt-kutular">
+
+<h3>TAXI DATABASE ( ONLY FOR DEVELOPMENT ISSUES )  </h3>
+<a href="index.php"> GO TO QUERY TAXI</a>
+
+
+
+<?php
+/*Connect using SQL Server authentication.*/
+
+$serverName = "tcp:mssmssdb.database.windows.net,1433";
+$connectionOptions = array("Database"=>"mssdb",
+                           "UID"=>"korkutanapa@mssmssdb",
+                           "PWD" => "774761Ka.");
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if($conn === false)
+{
+    die(print_r(sqlsrv_errors(), true));
+}
+
+
+/*Display registered people.*/
+$sql = "SELECT * FROM trip ORDER BY taxiplate";
+$stmt = sqlsrv_query($conn, $sql);
+if($stmt === false)
+{
+    die(print_r(sqlsrv_errors(), true));
+}
+if(sqlsrv_has_rows($stmt))
+{
+    print("<table border='1px'>");
+    print("<tr><td>Taxi Plate</td>");
+    print("<td>Point</td>");
+       
+    while($row = sqlsrv_fetch_array($stmt))
+    {
+         
+        print("<tr><td>".$row['taxiplate']."</td>");
+     
+        print("<td>".$row['star']."</td></tr>");
+    }
+    print("</table>");
+}
+
+
+?>
+
+
+</body>
+</div>
+</div>
+</html>
+
+
---
+++ anonymousrating.php
@@ -0,0 +1,150 @@
+
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/></head>
+
+<style type="text/css">
+#flex-kutu{
+width:auto;
+height:auto;
+display:flex;
+border:2px solid silver;
+flex-direction:row;
+background-color:#FFA500;
+}
+
+
+h1 {
+font-size: 80px;
+font-family: Comic Sans MS;
+color: blue;
+text-align:center;
+}
+
+p{
+margin-left: 50px;
+font-size: 30px;
+font-family: comic sans ms;
+color: black;
+}
+.alt-kutular{
+width:1500;
+margin:30px;
+padding:5px;
+background-color:#D3D3D3;
+background-repeat:no-repeat;
+
+}
+
+.alt-kutularana{
+width:1500;
+margin:30px;
+padding:5px;
+background-color:#D3D3D3;
+background-image: url("ELLER11.jpg");
+background-position:center;
+background-repeat:no-repeat;
+}
+
+</style>
+
+<body>
+<div id ="flex-kutu">
+
+<div class="alt-kutularana">
+
+<h3>ANONYMOUS RATING OF A TAXI  </h3>
+
+</div>
+
+
+<div class="alt-kutular">
+
+<h3>RATE THE TAXI GIVE A STAR  </h3>
+<a href="index.php"> GO TO QUERY TAXI</a>
+
+<form method="post" action="?action=add" enctype="multipart/form-data" >
+    Taxi Plate <input type="text" name="t_a" id="t_a"/></br>
+    Point <input type="text" name="t_b" id="t_b"/></br>
+        <input type="submit" name="submit" value="Submit" />
+</form>
+
+<?php
+/*Connect using SQL Server authentication.*/
+
+$serverName = "tcp:mssmssdb.database.windows.net,1433";
+$connectionOptions = array("Database"=>"mssdb",
+                           "UID"=>"korkutanapa@mssmssdb",
+                           "PWD" => "774761Ka.");
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if($conn === false)
+{
+    die(print_r(sqlsrv_errors(), true));
+}
+
+if(isset($_GET['action']))
+{
+    if($_GET['action'] == 'add')
+    {
+        /*Insert data.*/
+     $insertSql = "INSERT INTO [dbo].[trip] ([taxiplate],[star]) VALUES (?,?)";
+     $params = array(	&$_POST['t_a'],
+             
+                        &$_POST['t_b']
+					);
+					
+        $stmt = sqlsrv_query($conn, $insertSql, $params);
+        if($stmt === false)
+        {
+            /*Handle the case of a duplicte e-mail address.*/
+            $errors = sqlsrv_errors();
+            if($errors[0]['code'] == 2601)
+            {
+                echo "The e-mail address you entered has already been used.</br>";
+            }
+            /*Die if other errors occurred.*/
+            else
+            {
+                die(print_r($errors, true));
+            }
+        }
+        else
+        {
+            echo "Registration complete.</br>";
+        }
+    }
+}
+
+
+/*Display registered people.*/
+$sql = "SELECT * FROM trip ORDER BY taxiplate";
+$stmt = sqlsrv_query($conn, $sql);
+if($stmt === false)
+{
+    die(print_r(sqlsrv_errors(), true));
+}
+if(sqlsrv_has_rows($stmt))
+{
+    print("<table border='1px'>");
+    print("<tr><td>Taxi Plate</td>");
+    print("<td>Point</td>");
+       
+    while($row = sqlsrv_fetch_array($stmt))
+    {
+         
+        print("<tr><td>".$row['taxiplate']."</td>");
+     
+        print("<td>".$row['star']."</td></tr>");
+    }
+    print("</table>");
+}
+
+
+?>
+
+
+</body>
+</div>
+</div>
+</html>
+
+
