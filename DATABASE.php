02220c4ee282a87a2207e6c9f7243f8c2a2e5b3e
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-03-29 19:57:12 +0300

    Add files via upload

--- DATABASE.php
+++ DATABASE.php
@@ -103,13 +103,33 @@ if(sqlsrv_has_rows($stmt))
     print("<table border='1px'>");
     print("<tr><td>Taxi Plate</td>");
     print("<td>Point</td>");
+	print("<td>Date of Trip</td>");
+	print("<td>Time of Trip</td>");
+	print("<td>Location of Trip</td>");
+	print("<td>Customer</td>");
+	print("<td>Weather</td>");
+	print("<td>Feedback</td>");
+	print("<td>Complaint</td></tr>");
+	
+	
+	
+	
        
     while($row = sqlsrv_fetch_array($stmt))
     {
          
         print("<tr><td>".$row['taxiplate']."</td>");
-     
-        print("<td>".$row['star']."</td></tr>");
+        print("<td>".$row['star']."</td>");
+		print("<td>".$row['tripdate']."</td>");
+		print("<td>".$row['triptime']."</td>");
+		print("<td>".$row['triplocationin']."</td>");
+		print("<td>".$row['username']."</td>");
+		print("<td>".$row['weather']."</td>");
+		print("<td>".$row['feedback']."</td>");
+		print("<td>".$row['complaint']."</td></tr>");
+		
+		
+				
     }
     print("</table>");
 }
