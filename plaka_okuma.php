21c35db88ede7a377bf616b658b86a405fbb7140
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-04-04 13:04:36 +0300

    Add files via upload

--- plaka_okuma.php
+++ plaka_okuma.php
@@ -12,18 +12,19 @@ $data = base64_encode($img);
 // Display the output 
 echo $data; 
 ?> 
-   
 
-   <head>
+
+<head>
         <script>
         // Open connection to api.openalpr.com
         var secret_key = "sk_7e23fe78c0d325918f2c9f08";
         var url = "https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=us&secret_key=" + secret_key;
         var xhr = new XMLHttpRequest();
+		var data= "$data"
         xhr.open("POST", url);
 
         // Send POST data and display response
-		xhr.send("$data");
+		xhr.send("data");
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4) {
                 document.getElementById("response").innerHTML = xhr.responseText;
@@ -32,7 +33,7 @@ echo $data;
             }
         }
         </script>
-    </head>
+</head>
 
     <body>
         JSON response: <p id="response"></p><br>
