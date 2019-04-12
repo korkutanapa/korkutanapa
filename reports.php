1a82a6746e6052b2dcbcf9ea8abf09458e00adef
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-04-12 23:05:48 +0300

    Add files via upload

--- reports.php
+++ reports.php
@@ -135,25 +135,23 @@ if(sqlsrv_has_rows($stmt))
 		
     }
     print("</table><br>");
-}
+};
 
 
 
-$dataPoints = array(
-	array("x"=> 10, "y"=> 41),
-	array("x"=> 20, "y"=> 35, "indexLabel"=> "Lowest"),
-	array("x"=> 30, "y"=> 50),
-	array("x"=> 40, "y"=> 45),
-	array("x"=> 50, "y"=> 52),
-	array("x"=> 60, "y"=> 68),
-	array("x"=> 70, "y"=> 38),
-	array("x"=> 80, "y"=> 71, "indexLabel"=> "Highest"),
-	array("x"=> 90, "y"=> 52),
-	array("x"=> 100, "y"=> 60),
-	array("x"=> 110, "y"=> 36),
-	array("x"=> 120, "y"=> 49),
-	array("x"=> 130, "y"=> 41)
-);
+
+
+if(sqlsrv_has_rows($stmt))
+
+{  while($row = sqlsrv_fetch_array($stmt))
+    {
+    $jsonArrayItem = array();
+    $jsonArrayItem['x'] = $row['tr'];
+    $jsonArrayItem['y'] = $row['th'];
+    array_push($dataPoints,$jsonArrayItem);   
+}};
+
+
 
 
 
