40a91797105b1c9166c8927df71fb5f4f1d12180
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-04-12 22:33:56 +0300

    Add files via upload

--- reports.php
+++ reports.php
@@ -136,6 +136,35 @@ if(sqlsrv_has_rows($stmt))
     }
     print("</table><br>");
 }
+
+
+//storing the result of the executed query
+$result = $stmt;
+
+//initialize the array to store the processed data
+$jsonArray = array();
+
+//check if there is any data returned by the SQL Query
+if ($result->num_rows > 0) {
+  //Converting the results into an associative array
+  while($row = $result->fetch_assoc()) {
+    $jsonArrayItem = array();
+    $jsonArrayItem['label'] = $row['player'];
+    $jsonArrayItem['value'] = $row['wickets'];
+    //append the above created object into the main array.
+    array_push($jsonArray, $jsonArrayItem);
+  }
+}
+
+//Closing the connection to DB
+$conn->close();
+
+//set the response content type as JSON
+header('Content-type: application/json');
+//output the return value of json encode using the echo function. 
+echo json_encode($jsonArray);
+
+
 ?>
 
 
