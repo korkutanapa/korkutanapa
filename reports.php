b10800b3d1864e6ee0fa56e1792439e502d9aa66
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-05-07 08:52:55 +0300

    Add files via upload

--- reports.php
+++ reports.php
@@ -21,9 +21,9 @@ text-align:center;
 }
 
 p{
-margin-left: 50px;
-font-size: 30px;
-font-family: Times New Roman;
+
+font-size: 15px;
+font-family: Verdana ;
 color: black;
 }
 .alt-kutular{
@@ -70,16 +70,16 @@ background-repeat:no-repeat;
 
 <div class="alt-kutular">
 <h3>STANDART REPORTS  </h3>
-<h5> This page is for REPORTS</h5>
-<h5> You can take the results of the reports from the list below</h5>
-<h5> Reports can be taken according to:</h5>
-<h5>DAY OF THE WEEK ( please note that 1: SUNDAY 2: MONDAY .... 7: SATURDAY)</h5>
-<h5>HOUR OF THE DAY</h5>
-<h5>LOCATION</h5>
-<h5>USERS</h5>
-<h5>WEATHER TYPE</h5>
-<h5>SATISFACTION TYPE</h5>
-<h5>COMPLAINTS TYPE</h5>
+<p> This page is for REPORTS<br>
+You can take the results of the reports from the list below<br>
+Reports can be taken according to:<br>
+DAY OF THE WEEK ( please note that 1: SUNDAY 2: MONDAY .... 7: SATURDAY)<br>
+HOUR OF THE DAY<br>
+LOCATION<br>
+USERS<br>
+WEATHER TYPE<br>
+SATISFACTION TYPE<br>
+COMPLAINTS TYPE</p>
 
 <form method="post" action="?action=report" enctype="multipart/form-data" >
 
