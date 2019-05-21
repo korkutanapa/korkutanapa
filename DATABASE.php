ea39d2c65c3fbe58f03394e1dd03043d40818760
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-05-21 13:52:03 +0300

    Add files via upload

--- DATABASE.php
+++ DATABASE.php
@@ -101,8 +101,9 @@ th {
  <p>by BECKS</p>
 </header>
 
-			<section>
-					<nav>
+<section>
+
+<nav>
 
 <form id="form1" name="form1" method="post" action="logout.php">
 <p>
@@ -132,7 +133,7 @@ echo "<p>Hello  ".$_SESSION["username"]." you are wellcome</p> ";
 
 <p> Please enter your evaluation about your taxi trip </p>
 
-<form method="post" action="?action=add" enctype="multipart/form-data" >
+<form method="post" action="?action=addd" enctype="multipart/form-data" >
     Taxi Plate (format must be 01T0001)<br><input type="text" autocomplete="off" pattern="[0-9]{2}[A-Z]{1}[0-9]{4}" name="t_a" id="t_a"/></br>
 
 	Point or stars<br><input list="stars" autocomplete="off" name="t_b" >
@@ -194,7 +195,7 @@ if($conn === false)
 
 if(isset($_GET['action']))
 {
-    if($_GET['action'] == 'add')
+    if($_GET['action'] == 'addd')
     {
         /*Insert data.*/
      $insertSql = "INSERT INTO [dbo].[trip] ([taxiplate],[star],[tripdate],[triptime],[triplocationin],[username],[weather],[feedback],[complaint]) VALUES (?,?,?,?,?,?,?,?,?)";
@@ -239,7 +240,7 @@ if(isset($_GET['action']))
 
 ?>
 </article>
-			</section>
+</section>
 
 <footer>
   <div id="share-buttons">
