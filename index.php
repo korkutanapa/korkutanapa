ee01e33112dd055e41cfa229ad65aa0032ff8cff
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-10-22 21:42:32 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -104,10 +104,7 @@ th {
 
 <section>
 <nav>
-<form id="form1" name="form1" method="post" action="previousalarms.php">
-<p>
-<input style="background-color:#D3D3D3;width:350px;height:40px;font-size:16pt;margin-left:20px;font-family: Times New Roman;" type="submit" name="Submit" id="button" value="PREVIOUS ALARMS " />
-</p>
+
 
 
 <h3>SOLVED ALARMS</h3>
@@ -161,6 +158,12 @@ else{
 <article>
 
 <h3>ALARMS</h3>
+<form id="form1" name="form1" method="post" action="previousalarms.php">
+<p>
+<input style="background-color:#D3D3D3;width:350px;height:40px;font-size:16pt;margin-left:20px;font-family: Times New Roman;" type="submit" name="Submit" id="button" value="PREVIOUS ALARMS " />
+</p>
+
+
 <?php
 /*Connect using SQL Server authentication.*/
 
