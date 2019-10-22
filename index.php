2c32d083e5ee2c66554c08743a8222c1f31347f3
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-10-22 21:40:39 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ if(isset($_GET['action']))
 		
 	
 		
-$sql = "UPDATE  [dbo].[preses] SET completed='1' WHERE Id=('$a')";
+$sql = "UPDATE  [dbo].[preses] SET completed='1' WHERE alarmno=('$a')";
 $stmt = sqlsrv_query($conn, $sql);
 if($stmt === false)
 {
