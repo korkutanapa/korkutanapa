bb6c263f03047acdcdd08617c12809a69ee71233
korkutanapa <48103697+korkutanapa@users.noreply.github.com>
2019-12-14 12:43:13 +0300

    Add files via upload

--- previousalarms.php
+++ previousalarms.php
@@ -183,7 +183,7 @@ if(isset($_GET['action']))
 		$a=$_POST['t_aa'];
 
 
-$sqlA = "SELECT temp as temp, alarmdate as alarmdate,  FROM preses WHERE deviceId='$a' ";
+$sqlA = "SELECT T1 as temp, alarmdate as alarmdate,  FROM tresholdalarms WHERE deviceId='$a' ";
 $stmtA = sqlsrv_query($conn,$sqlA);
 
 $dataPoints=array();
